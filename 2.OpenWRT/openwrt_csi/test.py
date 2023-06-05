2d3651ccae2f2cb4a7903259f09d63dd26f964ea
Orlando Chen <seagochen@hotmail.com>
2023-06-05 23:30:19 +0900

    renamed the sub-project to Intel5300

--- 2.OpenWRT/openwrt_csi/test.py
+++ 2.OpenWRT/openwrt_csi/test.py
@@ -1,4 +1,4 @@
-from Plotter import plot_csi, plot_single_csi
+from AtheroPlotter import plot_csi, plot_single_csi
 
 
 from ReadLogFile import read_log_file
@@ -7,5 +7,5 @@ from ReadLogFile import read_log_file
 sample_data = read_log_file('csi.dat')
 signal_frame = sample_data[0]
 
-plot_single_csi(signal_frame)
+plot_csi(signal_frame, "amplitude")
 
