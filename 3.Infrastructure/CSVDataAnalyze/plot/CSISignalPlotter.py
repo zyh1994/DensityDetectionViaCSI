1904d26438009b56f287046ca44cd185ee097dc7
Orlando Chen <seagochen@hotmail.com>
2023-07-04 13:18:05 +0900

    ver 3 of replay the csi data done

--- 3.Infrastructure/CSVDataAnalyze/plot/CSISignalPlotter.py
+++ 3.Infrastructure/CSVDataAnalyze/plot/CSISignalPlotter.py
@@ -1,38 +1,35 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from io import BytesIO
 
 
-def update_dynamic_chart(ax, csi_matrix, chart_title, x_label, y_label, pause_time=0.01):
+def prepare_chart(title):
+    # Set up real-time plotting
+    plt.switch_backend('Agg')  # 设置Matplotlib的后端为非交互式模式
+
+    # Create a figure and an axis
+    fig, ax = plt.subplots()
+
+    # Change the figure title
+    # fig.canvas.manager.set_window_title(title)
+
+    return ax
+
+
+def update_chart(ax, csi_matrix):
     # Clear the previous plot
     ax.clear()
-
-    # Set the legend and the axis labels
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-
-    # Set the title
-    ax.set_title(chart_title)
 
     # Plot the CSI matrix
     ax.plot(csi_matrix)
 
-    # Pause for 0.01 second
-    plt.pause(0.01)
-
-
-def update_dynamic_vid_chart(ax, img, csi_matrix, chart_title, x_label, y_label, pause_time=0.01):
-    # Clear the previous plot
-    ax.clear()
-
     # Set the legend and the axis labels
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.set_xlabel('Subcarrier index')
+    ax.set_ylabel('SNR [dB]')
 
-    # Set the title
-    ax.set_title(chart_title)
+    # Save the plot to a BytesIO object
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
 
-    # Plot the CSI matrix
-    ax.imshow(img, cmap='gray')
-
-    # Pause for 0.01 second
-    plt.pause(0.01)
+    # Return the BytesIO object
+    return buffer
--- 3.Infrastructure/CSVDataAnalyze/plot/MatrixPreprocessor.py
+++ 3.Infrastructure/CSVDataAnalyze/plot/MatrixPreprocessor.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def preprocess_original_matrix(csi_matrix):
+def preprocess_matrix(csi_matrix):
     # combine the real and imaginary part of the CSI matrix to form a complex number
     csi_matrix = csi_matrix[:, :, :, 0] + 1j * csi_matrix[:, :, :, 1]
 
     # get the maximum value of the CSI matrix
-    max_csi = np.max(csi_matrix)
+    max_csi = np.max(csi_matrix) + 1e-10
 
     # get the minimum value of the CSI matrix
     min_csi = np.min(csi_matrix)
@@ -17,13 +17,7 @@ def preprocess_original_matrix(csi_matrix):
     return csi_matrix
 
 
-def get_amplitude_matrix(csi_matrix):
-
-    # get the number of receive antennas, transmit antennas and subcarriers
-    nr, nc, tones = csi_matrix.shape
-
-    # convert the shape of the CSI matrix to (channels, subcarriers)
-    csi_matrix = np.reshape(csi_matrix, (nr * nc, tones))
+def amplitude_matrix(csi_matrix):
 
     # get the amplitude of the CSI matrix
     csi_matrix = np.abs(csi_matrix)
@@ -31,30 +25,17 @@ def get_amplitude_matrix(csi_matrix):
     return csi_matrix
 
 
-def get_db_matrix(csi_matrix):
-
-    # get the number of receive antennas, transmit antennas and subcarriers
-    nr, nc, tones = csi_matrix.shape
-
-    # convert the shape of the CSI matrix to (channels, subcarriers)
-    csi_matrix = np.reshape(csi_matrix, (nr * nc, tones))
-
+def db_matrix(csi_matrix):
     # get the amplitude of the CSI matrix
-    csi_matrix = np.abs(csi_matrix)
+    csi_matrix_abs = np.abs(csi_matrix)
 
     # convert the amplitude to dB
-    csi_matrix = 20 * np.log10(csi_matrix)
+    csi_matrix_db = 20 * np.log10(csi_matrix_abs)
 
-    return csi_matrix
+    return csi_matrix_db
 
 
-def get_phase_matrix(csi_matrix):
-
-    # get the number of receive antennas, transmit antennas and subcarriers
-    nr, nc, tones = csi_matrix.shape
-
-    # convert the shape of the CSI matrix to (channels, subcarriers)
-    csi_matrix = np.reshape(csi_matrix, (nr * nc, tones))
+def phase_matrix(csi_matrix):
 
     # get the phase of the CSI matrix
     csi_matrix = np.angle(csi_matrix)
