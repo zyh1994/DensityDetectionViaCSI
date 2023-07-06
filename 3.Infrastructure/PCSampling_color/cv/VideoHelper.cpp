f70f7bb494a0ad2ce5270b04c94a66516d30d4b5
Orlando Chen <seagochen@hotmail.com>
2023-07-06 16:37:05 +0900

    temp

--- 3.Infrastructure/PCSampling_color/cv/VideoHelper.cpp
+++ 3.Infrastructure/PCSampling_color/cv/VideoHelper.cpp
@@ -9,94 +9,41 @@
 using namespace cv;
 using namespace std;
 
-/**
- * @brief Calculate the average hash value of the given image.
- * @param src
- * @return
- */
-cv::Mat averageHash(const cv::Mat& src){
-    cv::Mat img, gray;
-    cv::resize(src, img, cv::Size(8, 8), 0, 0, cv::INTER_AREA);
-    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-    uchar avg = cv::mean(gray)[0];
-    cv::Mat hash = gray >= avg;
-    return hash;
-}
+namespace sge {
 
-/**
- * @brief Calculate the difference hash value of the given image.
- * @param src
- * @return
-*/
-cv::Mat differenceHash(const cv::Mat& src){
-    cv::Mat img, gray, diff;
-    cv::resize(src, img, cv::Size(9, 8), 0, 0, cv::INTER_AREA);
-    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-    cv::absdiff(gray(cv::Rect(1, 0, 8, 8)), gray(cv::Rect(0, 0, 8, 8)), diff);
-    cv::Mat hash = diff >= 0;
-    return hash;
-}
+    VideoCapture VideoHelper::openCamera(int cameraId) {
 
-/**
- * @brief Calculate the perceptual hash value of the given image.
- * @param src
- * @return
-*/
-cv::Mat perceptualHash(const cv::Mat& src){
-    cv::Mat img, gray, dct, hash;
-    cv::resize(src, img, cv::Size(32, 32), 0, 0, cv::INTER_AREA);
-    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-    cv::dct(cv::Mat_<float>(gray), dct);
-    dct = dct(cv::Rect(0, 0, 8, 8));
-    uchar avg = cv::mean(dct)[0];
-    hash = dct >= avg;
-    return hash;
-}
+        // Open the camera.
+        VideoCapture cap(cameraId);
 
-/**
- * @brief Calculate the Marr-Hildreth hash value of the given image.
- * @param src
- * @return
-*/
-std::string hashToString(const cv::Mat& hash){
-    std::string hashString;
-    for (int i = 0; i < hash.rows; ++i){
-        for (int j = 0; j < hash.cols; ++j){
-            hashString += std::to_string(hash.at<uchar>(i,j));
+        // Check the camera.
+        if (!cap.isOpened()) {
+            cout << "Cannot open the camera!" << endl;
+            exit(-1);
         }
+
+        // Return the camera.
+        return cap;
     }
-    return hashString;
-}
 
-VideoCapture VideoHelper::openCamera(int cameraId) {
-    VideoCapture cap(cameraId);
-    if (!cap.isOpened()) {
-        cout << "Cannot open the camera!" << endl;
-        exit(-1);
+    cv::Writer VideoHelper::openVideoWriter(const std::string &path, VideoTypeFourCC fourcc,
+                                            double fps, cv::Size frameSize) {
+
+        // Convert the fourcc to integer.
+        int fourccInt = static_cast<int>(fourcc);
+
+        // Check the fourcc.
+        VideoWriter writer(path, fourccInt, fps, frameSize);
+
+        // Check the writer.
+        if (!writer.isOpened()) {
+            cout << "Cannot open the video writer!" << endl;
+            exit(-1);
+        }
+
+        // Return the writer.
+        return writer;
     }
-    return cap;
-}
 
-VideoWriter VideoHelper::openVideoWriter(const string &filename, int codec, double fps, Size frameSize) {
+};
 
-    VideoWriter writer(filename, codec, fps, frameSize);
-    if (!writer.isOpened()) {
-        cout << "Cannot open the video writer!" << endl;
-        exit(-1);
-    }
-    return writer;
-}
-
-std::string VideoHelper::getHashValue(const Mat &image, const string &hashFunction) {
-
-    if (hashFunction == "AverageHash"){
-        return hashToString(averageHash(image));
-    } else if (hashFunction == "DifferenceHash"){
-        return hashToString(differenceHash(image));
-    } else if (hashFunction == "PerceptualHash"){
-        return hashToString(perceptualHash(image));
-    } else {
-        cout << "The hash function is not supported!" << endl;
-        exit(-1);
-    }
-}
--- 3.Infrastructure/PCSampling_color/cv/VideoHelper.h
+++ 3.Infrastructure/PCSampling_color/cv/VideoHelper.h
@@ -8,36 +8,53 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
-class VideoHelper {
+namespace sge {
 
-public:
+    enum VideoTypeFourCC {
 
-    /**
-     * Open the camera with the given id.
-     * Normally, the id is default to 0.
-     * @param cameraId
-     * @return
-     */
-    static cv::VideoCapture openCamera(int cameraId = 0);
+        // H.264/AVC codec
+        H264 = cv::VideoWriter::fourcc('H', '2', '6', '4'),
 
-    /**
-     * Open the video file with the given filename.
-     * @param filename
-     * @param codec
-     * @param fps
-     * @param frameSize
-     * @return
-     */
-    static cv::VideoWriter openVideoWriter(const std::string &filename, int codec, double fps, cv::Size frameSize);
+        // H.265/HEVC codec
+        H265 = cv::VideoWriter::fourcc('H', 'E', 'V', 'C'),
 
-    /**
-     * Get the hash value of the given image.
-     * @param image
-     * @param hashFunction
-     * @return
-     */
-    static std::string getHashValue(const cv::Mat& image, const std::string &hashFunction = "AverageHash");
-};
+        // MPEG4-based
+        MPEG_4 = cv::VideoWriter::fourcc('M', 'P', '4', 'V'),
 
+        // Motion JPEG codec
+        MJPEG = cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+
+        // Uncompressed YUV format
+        YUV = cv::VideoWriter::fourcc('I', '4', '2', '0'),
+
+        // Windows Media Video (WMV) 1 codec
+        WMV1 = cv::VideoWriter::fourcc('W', 'M', 'V', '1'),
+    };
+
+
+    class VideoHelper {
+
+    public:
+
+        /**
+         * Open the camera with the given id.
+         * Normally, the id is default to 0.
+         * @param cameraId
+         * @return
+         */
+        static cv::VideoCapture openCamera(int cameraId = 0);
+
+        /**
+         * Open the video file with the given path.
+         * @param path
+         * @param fourcc
+         * @param fps
+         * @param frameSize
+         */
+        static cv::Writer openVideoWriter(const std::string &path, VideoTypeFourCC fourcc,
+                                          double fps, cv::Size frameSize);
+
+    };
+}
 
 #endif //DENSITYDETECTION_VIDEOHASH_H
